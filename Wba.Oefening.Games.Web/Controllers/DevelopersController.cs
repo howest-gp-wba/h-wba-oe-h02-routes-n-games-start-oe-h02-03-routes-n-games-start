d6a73a676c5d2a39ec2d6ea0cf08f337c853216b
LoesCc <loes.coucke@student.howest.be>
2024-11-19 09:28:01 +0100

    Add DevelopersController

---
+++ Wba.Oefening.Games.Web/Controllers/DevelopersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wba.Oefening.Games.Web.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
